3fb4c71ee6922721b234d53e1d2f981e2ca32122
EricKalinowski <eric_manutd7@hotmail.es>
2015-10-19 18:06:55 -0300

    listado estadistico

---
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Listado Estadistico/Listado.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AerolineaFrba.Listado_Estadistico
+{
+    partial class Listado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxFiltro1 = new System.Windows.Forms.TextBox();
+            this.textBoxFiltro2 = new System.Windows.Forms.TextBox();
+            this.textBoxFiltro2Seleccionar = new System.Windows.Forms.TextBox();
+            this.comboBoxFiltro3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Filtros de Busqueda";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Filtro 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Filtro 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(303, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Filtro 3";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(303, 74);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Filtro 4";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Seleccionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(38, 114);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpiar.TabIndex = 10;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(461, 114);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuscar.TabIndex = 11;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 162);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 237);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // textBoxFiltro1
+            // 
+            this.textBoxFiltro1.Location = new System.Drawing.Point(58, 43);
+            this.textBoxFiltro1.Name = "textBoxFiltro1";
+            this.textBoxFiltro1.Size = new System.Drawing.Size(239, 20);
+            this.textBoxFiltro1.TabIndex = 13;
+            // 
+            // textBoxFiltro2
+            // 
+            this.textBoxFiltro2.Location = new System.Drawing.Point(58, 71);
+            this.textBoxFiltro2.Name = "textBoxFiltro2";
+            this.textBoxFiltro2.Size = new System.Drawing.Size(239, 20);
+            this.textBoxFiltro2.TabIndex = 14;
+            // 
+            // textBoxFiltro2Seleccionar
+            // 
+            this.textBoxFiltro2Seleccionar.Location = new System.Drawing.Point(347, 71);
+            this.textBoxFiltro2Seleccionar.Name = "textBoxFiltro2Seleccionar";
+            this.textBoxFiltro2Seleccionar.ReadOnly = true;
+            this.textBoxFiltro2Seleccionar.Size = new System.Drawing.Size(139, 20);
+            this.textBoxFiltro2Seleccionar.TabIndex = 15;
+            // 
+            // comboBoxFiltro3
+            // 
+            this.comboBoxFiltro3.FormattingEnabled = true;
+            this.comboBoxFiltro3.Location = new System.Drawing.Point(347, 44);
+            this.comboBoxFiltro3.Name = "comboBoxFiltro3";
+            this.comboBoxFiltro3.Size = new System.Drawing.Size(220, 21);
+            this.comboBoxFiltro3.TabIndex = 16;
+            // 
+            // Listado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 411);
+            this.Controls.Add(this.comboBoxFiltro3);
+            this.Controls.Add(this.textBoxFiltro2Seleccionar);
+            this.Controls.Add(this.textBoxFiltro2);
+            this.Controls.Add(this.textBoxFiltro1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Listado";
+            this.Text = "Listado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBoxFiltro1;
+        private System.Windows.Forms.TextBox textBoxFiltro2;
+        private System.Windows.Forms.TextBox textBoxFiltro2Seleccionar;
+        private System.Windows.Forms.ComboBox comboBoxFiltro3;
+
+    }
+}
---
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Listado Estadistico/Seleccionar.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace AerolineaFrba.Listado_Estadistico
+{
+    partial class Seleccionar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxFiltro3 = new System.Windows.Forms.ComboBox();
+            this.textBoxFiltro2Seleccionar = new System.Windows.Forms.TextBox();
+            this.textBoxFiltro2 = new System.Windows.Forms.TextBox();
+            this.textBoxFiltro1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxFiltro3
+            // 
+            this.comboBoxFiltro3.FormattingEnabled = true;
+            this.comboBoxFiltro3.Location = new System.Drawing.Point(343, 43);
+            this.comboBoxFiltro3.Name = "comboBoxFiltro3";
+            this.comboBoxFiltro3.Size = new System.Drawing.Size(220, 21);
+            this.comboBoxFiltro3.TabIndex = 29;
+            // 
+            // textBoxFiltro2Seleccionar
+            // 
+            this.textBoxFiltro2Seleccionar.Location = new System.Drawing.Point(343, 70);
+            this.textBoxFiltro2Seleccionar.Name = "textBoxFiltro2Seleccionar";
+            this.textBoxFiltro2Seleccionar.ReadOnly = true;
+            this.textBoxFiltro2Seleccionar.Size = new System.Drawing.Size(139, 20);
+            this.textBoxFiltro2Seleccionar.TabIndex = 28;
+            // 
+            // textBoxFiltro2
+            // 
+            this.textBoxFiltro2.Location = new System.Drawing.Point(54, 70);
+            this.textBoxFiltro2.Name = "textBoxFiltro2";
+            this.textBoxFiltro2.Size = new System.Drawing.Size(239, 20);
+            this.textBoxFiltro2.TabIndex = 27;
+            // 
+            // textBoxFiltro1
+            // 
+            this.textBoxFiltro1.Location = new System.Drawing.Point(54, 42);
+            this.textBoxFiltro1.Name = "textBoxFiltro1";
+            this.textBoxFiltro1.Size = new System.Drawing.Size(239, 20);
+            this.textBoxFiltro1.TabIndex = 26;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 161);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 237);
+            this.dataGridView1.TabIndex = 25;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Location = new System.Drawing.Point(457, 113);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBuscar.TabIndex = 24;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(34, 113);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpiar.TabIndex = 23;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Seleccionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(299, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Filtro 4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(299, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Filtro 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Filtro 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Filtro 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Filtros de Busqueda";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(336, 236);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 30;
+            this.monthCalendar1.Visible = false;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Seleccionar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 414);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.comboBoxFiltro3);
+            this.Controls.Add(this.textBoxFiltro2Seleccionar);
+            this.Controls.Add(this.textBoxFiltro2);
+            this.Controls.Add(this.textBoxFiltro1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Seleccionar";
+            this.Text = "Seleccionar";
+            this.Load += new System.EventHandler(this.Seleccionar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxFiltro3;
+        private System.Windows.Forms.TextBox textBoxFiltro2Seleccionar;
+        private System.Windows.Forms.TextBox textBoxFiltro2;
+        private System.Windows.Forms.TextBox textBoxFiltro1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button button2;
+    }
+}

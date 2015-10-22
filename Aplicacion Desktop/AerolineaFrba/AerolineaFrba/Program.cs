1370ec5c7a53774da6c379bdf474523276f10504
EricKalinowski <eric_manutd7@hotmail.es>
2015-10-22 15:40:40 -0300

    Arreglo listados de aeronave y de roles

--- Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Program.cs
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AerolineaFrba;
 
 namespace AerolineaFrba
 {
@@ -17,7 +18,7 @@ namespace AerolineaFrba
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
           //  Application.Run(new Form1());
-            Application.Run(new Form1());
+            Application.Run(new Abm_Aeronave.ListadoAeronave());
         }
     }
 }

e0ad0f7a7ebdd3a16684fe2e8d8e071e0ef981a8
Leonardo-Rocca <leonardo_7@live.com.ar>
2015-10-26 19:08:30 -0300

    muestra error en el login

--- Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Login/LoginAdministrador.cs
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Login/LoginAdministrador.cs
@@ -52,9 +52,9 @@ namespace AerolineaFrba.Login
                 redirec.Show();
                 this.Hide();
                       }
-                      catch
+                      catch(Exception er)
                       {
-                          MessageBox.Show("El usuario y contraseña no coinciden");
+                          MessageBox.Show(er.Message );
                       }
             
             }

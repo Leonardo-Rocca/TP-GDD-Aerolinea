50c1a0967daa2e3a433abe1f72a45161bc544021
Leonardo-Rocca <leonardo_7@live.com.ar>
2015-10-26 14:15:22 -0300

    agrego ABM ciudades

---
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Abm Ciudad/ModificarCiudadForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AerolineaFrba.Abm_Ciudad
+{
+    partial class ModificarCiudadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbtitulo = new System.Windows.Forms.Label();
+            this.txtNombreCiudad = new System.Windows.Forms.TextBox();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbtitulo
+            // 
+            this.lbtitulo.AutoSize = true;
+            this.lbtitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtitulo.Location = new System.Drawing.Point(21, 25);
+            this.lbtitulo.Name = "lbtitulo";
+            this.lbtitulo.Size = new System.Drawing.Size(180, 25);
+            this.lbtitulo.TabIndex = 3;
+            this.lbtitulo.Text = "Modificar Ciudad:";
+            // 
+            // txtNombreCiudad
+            // 
+            this.txtNombreCiudad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreCiudad.Location = new System.Drawing.Point(39, 161);
+            this.txtNombreCiudad.Name = "txtNombreCiudad";
+            this.txtNombreCiudad.Size = new System.Drawing.Size(536, 26);
+            this.txtNombreCiudad.TabIndex = 12;
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGuardar.Location = new System.Drawing.Point(449, 268);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(117, 58);
+            this.buttonGuardar.TabIndex = 16;
+            this.buttonGuardar.Text = "Modificar City";
+            this.buttonGuardar.UseVisualStyleBackColor = true;
+            this.buttonGuardar.Click += new System.EventHandler(this.buttonGuardar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancelar.Location = new System.Drawing.Point(66, 268);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(117, 58);
+            this.buttonCancelar.TabIndex = 15;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(47, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Elegir el nuevo nombre de la ciudad:";
+            // 
+            // ModificarCiudadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 364);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.txtNombreCiudad);
+            this.Controls.Add(this.lbtitulo);
+            this.Name = "ModificarCiudadForm";
+            this.Text = "ModificarCiudadForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbtitulo;
+        private System.Windows.Forms.TextBox txtNombreCiudad;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonCancelar;
+        private System.Windows.Forms.Label label3;
+    }
+}
--- Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Abm Ciudad/crearCiudadForm.cs
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Abm Ciudad/crearCiudadForm.cs
@@ -25,6 +25,18 @@ namespace AerolineaFrba.Abm_Ciudad
                 MessageBox.Show("Falta agregar nombre", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            DataTable dt = (new ConexionSQL()).cargarTablaSQL("select distinct nombre_ciudad FROM  DBAS.ciudades WHERE nombre_ciudad LIKE '_" + txtNombreCity.Text + "'OR nombre_ciudad LIKE '" + txtNombreCity.Text + "' ");
+            //--pregunto si hay alguna fila cuyo nombre coincida con el ingresado-- 
+            if (dt.Rows.Count == 0)
+            {
+                //TO-DO
+                MessageBox.Show("Ciudad Agregada (DAMMY)", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                MessageBox.Show("La ciudad ya existe", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
             MessageBox.Show("Ciudad agregada", this.Text, MessageBoxButtons.OK, MessageBoxIcon.None);
             this.Hide(); //.Close();
             txtNombreCity.Text = "";

c1234a53d10a33db9ab7dc443bf72dfcb2e9dbe2
EricKalinowski <eric_manutd7@hotmail.es>
2015-11-04 15:53:40 -0300

    Cancelacion de pasajes (subo el form)

---
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Devolucion/Devolucion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Devolucion
+{
+    public partial class Devolucion : Form
+    {
+        public Devolucion()
+        {
+            InitializeComponent();
+            iniciar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.iniciar();
+            this.Hide();
+        }
+
+        private void iniciar()
+        {
+            textBox0.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            dateFecha.Value = DateTime.Today;
+          
+        }
+
+    }
+}

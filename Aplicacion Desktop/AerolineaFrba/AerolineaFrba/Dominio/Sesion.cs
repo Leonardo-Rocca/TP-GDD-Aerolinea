cdc74ce7bf0e6fc082ef5b99357f4819fa8a571a
Leonardo-Rocca <leonardo_7@live.com.ar>
2015-11-05 17:19:40 -0300

    solo aparecen lasfuncionalidades asociadas

--- Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Dominio/Sesion.cs
+++ Aplicacion Desktop/AerolineaFrba/AerolineaFrba/Dominio/Sesion.cs
@@ -41,29 +41,60 @@ namespace AerolineaFrba.Dominio
             string qfuncion = "select * from  DBAS.obtenerFuncionalidadesAsociadas ('" + rol + "')";
             DataTable dtfunciones =  (new ConexionSQL()).cargarTablaSQL(qfuncion);
 
-                funcionalidadesDisponibles = new List<Funcionalidades>();
-            //--Creo y Agrego ABM ROL
-             funcionalidadesDisponibles.Add(new Funcionalidades(99, "ABM ROL",
-                 new ABMGenericoForm("ABM Rol", new CrearRolForm(), new ModificarForm("Modificar Rol", "Modificar", 1), new ModificarForm("Eliminar Rol", "Eliminar", 2))));
-
-             funcionalidadesDisponibles.Add(new Funcionalidades(99, "ABM Ciudades",
-                  new ABMGenericoForm("ABM Ciudades", new crearCiudadForm(), new modificarEliminarCityForm(1), new modificarEliminarCityForm(2) )));
-
-             funcionalidadesDisponibles.Add(new Funcionalidades(99, "ABM Rutas",
-                new ABMGenericoForm("ABM Rutas", new crearRutaForm(), new ModificarEliminarRuta(1), new ModificarEliminarRuta(2) )));
-
-             funcionalidadesDisponibles.Add(new Funcionalidades(99, "ABM Aeronave",
-               new ABMGenericoForm("ABM Aeronave", new AltaAeronave(), new ModificarAeronave(0), new ModificarAeronave(1))));
-
-             funcionalidadesDisponibles.Add(new Funcionalidades(99, "Generar Viaje",
-             new Generar_Viaje()));
-
-             funcionalidadesDisponibles.Add(new Funcionalidades(99, "Listado Estadistico",new Top5.DiscriminadorTop5()));
-
-            funcionalidadesDisponibles.Add(new Funcionalidades(99, "Comprar Pasaje/Encomienda", new SeleccionViajeForm()));
-
-            funcionalidadesDisponibles.Add(new Funcionalidades(99, "Consultar millas", new ConsultarMillasForm()));
+            cargarFuncionalidades(dtfunciones);
+            
         
+        }
+
+        private static void cargarFuncionalidades(DataTable dtfunciones)
+        {
+            funcionalidadesDisponibles = new List<Funcionalidades>();
+
+                for (int i = 0; i <= (dtfunciones.Rows.Count - 1); i++)
+            {
+                    funcionalidadesDisponibles.Add(obtenerFuncionalidad(dtfunciones.Rows[i][0].ToString() ));
+              }
+        }
+
+        private static Funcionalidades obtenerFuncionalidad(string descripcionF)
+        {
+            if(descripcionF=="ABM de Rol")
+             return (new Funcionalidades(99, "ABM ROL",
+                new ABMGenericoForm("ABM Rol", new CrearRolForm(), new ModificarForm("Modificar Rol", "Modificar", 1), new ModificarForm("Eliminar Rol", "Eliminar", 2))));
+               
+            if(descripcionF=="ABM de Ciudades")
+            return (new Funcionalidades(99, "ABM Ciudades",
+                 new ABMGenericoForm("ABM Ciudades", new crearCiudadForm(), new modificarEliminarCityForm(1), new modificarEliminarCityForm(2))));
+
+            if(descripcionF=="ABM de Ruta Aerea")
+            return (new Funcionalidades(99, "ABM Rutas",
+               new ABMGenericoForm("ABM Rutas", new crearRutaForm(), new ModificarEliminarRuta(1), new ModificarEliminarRuta(2))));
+
+            if (descripcionF == "ABM de Aeronaves")
+            return (new Funcionalidades(99, "ABM Aeronave",
+              new ABMGenericoForm("ABM Aeronave", new AltaAeronave(), new ModificarAeronave(0), new ModificarAeronave(1))));
+                      
+            if(descripcionF=="Generar Viaje")
+            return  (new Funcionalidades(99, "Generar Viaje", new Generar_Viaje()));
+
+            if (descripcionF == "Listado Estadístico")
+            return (new Funcionalidades(99, "Listado Estadistico", new Top5.DiscriminadorTop5()));
+
+             if (descripcionF == "Registro de llegada Destino")
+            return (new Funcionalidades(99, "Registro de llegada Destino", new Registro_Llegada_Destino.RegistroLLegadaForm()));
+
+               if (descripcionF == "Compra de Pasaje/Encomienda")
+            return (new Funcionalidades(99, "Comprar Pasaje/Encomienda", new SeleccionViajeForm()));
+            
+            if (descripcionF == "Consulta de millas de pasajero frecuente")
+            return (new Funcionalidades(99, "Consultar millas", new ConsultarMillasForm()));
+            //Registro de Usuario
+            //default..
+            return (new Funcionalidades(99, "Devolucion ", new Devolucion.Devolucion()));
+
+//Cancelación/Devolución de pasaje y/o encomienda
+//Canje de millas de pasajero frecuente
+
         }
 
         public static List<Funcionalidades> getFuncionalidadesDisponibles{
